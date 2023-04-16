156a7daaf6e71c332a5d58c4286292869caf66b5
edilbertocantuaria <edilbertounbfga@gmail.com>
2023-04-16 20:39:43 -0300

    Checking error!

--- src/app.js
+++ src/app.js
@@ -2,7 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import chalk from 'chalk';
 import dotenv from 'dotenv';
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
+import joi from 'joi';
+import dayjs from 'dayjs';
+import "dayjs/locale/pt-br.js";
+
+dayjs.locale('pt-br');
+const rigthNow = dayjs();
 
 const app = express();
 
@@ -10,14 +16,60 @@ app.use(express.json());
 app.use(cors())
 dotenv.config();
 
-
-
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
 let db;
 
-mongoClient.connect().then(() => {
-    db = mongoClient.db();
-}).catch((err) => console.log(err.message))
+mongoClient.connect()
+    .then(() => {
+        db = mongoClient.db();
+    })
+    .catch((err) => console.log(err.message))
+
+
+const userSchema = joi.object({
+    name: joi.string().required()
+})
+
+app.post("/participants", (req, res) => {
+    const { name } = req.body
+
+    const validation = userSchema.validate(req.body)
+    if (validation.error) return res.sendStatus(422)
+
+    //procurar alguma coisa no bd aqui!
+
+    const loginUser = db.collection("participants").insertOne({
+        name: name,
+        lastStatus: Date.now()
+    })
+    loginUser.then(() => res.sendStatus(201));
+    loginUser.catch(() => res.send("algum erro aconteceu!"))
+
+    const userLogged = db.collection("messages").insertOne({
+        from: name,
+        to: 'Todos',
+        text: 'entra na sala...',
+        type: 'status',
+        time: rigthNow.format('HH:mm:ss')
+    })
+
+});
+
+
+app.get("/participants", (req, res) => {
+
+    const promise = db.collection("participants").find({}).toArray();
+    promise.then(participantes => res.send(participantes));
+    promise.catch(() => res.send("Deu algum erro aqui!"));
+
+
+});
+
+app.post("/messages", (req, res) => { });
+app.get("/messages", (req, res) => { });
+
+app.post("/status", (req, res) => { });
+
 
 
 
